3d9d6908b35a328d8cdf2d1a7273b486b63fc228
abelass <rainer@websimple.be>
2017-01-05 15:59:30 +0100

    lisibilité code

--- cheques_cadeau_pipelines.php
+++ cheques_cadeau_pipelines.php
@@ -24,25 +24,25 @@ if (! defined('_ECRIRE_INC_VERSION'))
 function cheques_cadeau_affiche_milieu($flux) {
 	$texte = "";
 	$e = trouver_objet_exec($flux['args']['exec']);
-	
+
 	include_spip('inc/config');
-	
+
 	// cadeau_cheques sur les objets choisies
 	if (! $e['edition'] and in_array($e['table_objet_sql'], array_filter(lire_config('cheques_cadeau/objets', array ())))) {
 		$texte .= recuperer_fond('prive/objets/editer/liens', array (
 			'table_source' => 'cadeau_cheques',
 			'objet' => $e['type'],
-			'id_objet' => $flux['args'][$e['id_table_objet']] 
+			'id_objet' => $flux['args'][$e['id_table_objet']]
 		));
 	}
-	
+
 	if ($texte) {
 		if ($p = strpos($flux['data'], "<!--affiche_milieu-->"))
 			$flux['data'] = substr_replace($flux['data'], $texte, $p, 0);
 		else
 			$flux['data'] .= $texte;
 	}
-	
+
 	return $flux;
 }
 
@@ -61,26 +61,29 @@ function cheques_cadeau_affiche_milieu($flux) {
 function cheques_cadeau_post_edition($flux) {
 	$args = $flux['args'];
 	$data = $flux['data'];
-	
-	if ($args['action'] == 'instituer' 
-			and $args['table'] == 'spip_commandes' 
-			and $data['statut'] == 'paye' 
-			and $id_commande = $args['id_objet'] 
-			and $email_beneficiaire = sql_getfetsel('email_beneficiaire', 'spip_commandes', "id_commande=$id_commande AND source LIKE '%cheque_cadeau%'") and $notifications = charger_fonction('notifications', 'inc', true)) {
-		
+
+	if ($args['action'] == 'instituer'
+			and $args['table'] == 'spip_commandes'
+			and $data['statut'] == 'paye'
+			and $id_commande = $args['id_objet']
+			and $email_beneficiaire = sql_getfetsel(
+					'email_beneficiaire', 'spip_commandes',
+					"id_commande=$id_commande AND source LIKE '%cheque_cadeau%'")
+			and $notifications = charger_fonction('notifications', 'inc', true)) {
+
 		include_spip('inc/config');
 		$config = lire_config('commandes');
 		// Déterminer l'expéditeur
 		$options = array ();
-		
+
 		if ($config['expediteur'] != "facteur") {
 			$options['expediteur'] = $config['expediteur_' . $config['expediteur']];
 		}
-		
+
 		// Envoyer au beneficiaire
 		spip_log("traiter_notifications_commande : notification beneficiaire pour la commande $id_commande", 'commandes.' . _LOG_INFO);
 		$notifications('commande_beneficiaire', $id_commande, $options);
-		
+
 		// Si plugin crédit, enregistrer un crédit en faveur du bénéficiaire
 		if (test_plugin_actif('reservations_credits')) {
 			$action = charger_fonction('editer_objet', 'action');
@@ -91,7 +94,7 @@ function cheques_cadeau_post_edition($flux) {
 			$sql = sql_select('id_commandes_detail, prix_unitaire_ht, taxe, descriptif',
 					'spip_commandes_details',
 					'id_commande=' . $id_commande);
-					
+
 			while ( $data = sql_fetch($sql) ) {
 				set_request('id_objet', $data['id_commandes_detail']);
 				set_request('objet', 'commande');
@@ -121,7 +124,7 @@ function cheques_cadeau_post_edition($flux) {
 function cheques_cadeau_optimiser_base_disparus($flux) {
 	include_spip('action/editer_liens');
 	$flux['data'] += objet_optimiser_liens(array (
-		'cadeau_cheque' => '*' 
+		'cadeau_cheque' => '*'
 	), '*');
 	return $flux;
 }
